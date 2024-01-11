b824a537c52bf9c71cb601e1ead7a986378c740c
Laura Sirigu <laurasirigu43@gmail.com>
2024-01-11 19:32:59 +0100

    feat || primo bonus

--- index.js
+++ index.js
@@ -15,20 +15,33 @@ const exister = (path) => {
         exist = false;
     }
     return exist;
-}
+};
+
+const searchTypes = ['popular', 'top_rated', 'now_playing'];
+const arg = process.argv[2];
+
+
 
 
 (async () => {
 
     try{
-        if(!exister(moviesPath)){
-            const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
+        if(searchTypes.includes(arg)){
+            const response = await fetch(`https://api.themoviedb.org/3/movie/${arg}?api_key=${apiKey}`);
             const obj = await response.json();
             const moviesJson = JSON.stringify(obj.results);
             fs.writeFileSync(moviesPath, moviesJson);  
-            console.log('Fetch effettuato!')
+            console.log('arg fetch')
         }
 
+        else{
+            if(!exister(moviesPath)){
+                const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
+                const obj = await response.json();
+                const moviesJson = JSON.stringify(obj.results);
+                fs.writeFileSync(moviesPath, moviesJson);  
+                console.log('Fetch effettuato!')
+        }}
     }catch(error){console.error(error)};
    
 })()
